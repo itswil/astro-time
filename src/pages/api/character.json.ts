95e726827b993f71221927424a2e809432f862e3
itswil <ww8668@gmail.com>
2024-09-10 10:50:54 +0800

    Add cache to api

--- src/pages/api/character.json.ts
+++ src/pages/api/character.json.ts
@@ -3,5 +3,10 @@ export async function GET({ params, request }) {
 	const data = await response.json();
 	data.timestamp = new Date().toISOString();
 
-	return new Response(JSON.stringify(data));
+	return new Response(JSON.stringify(data), {
+		headers: {
+			"Netlify-CDN-Cache-Control":
+				"public, durable, max-age=20, stale-while-revalidate=40",
+		},
+	});
 }
